c4e3ac7750e1235e6e23e8b00c850e2cf415ca71
Mohamed <ramy.adam33@gmail.com>
2026-07-13 07:10:21 +0400

    fix: soften Classic ayah marker contrast (#45)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -950,7 +950,7 @@ void main() {
       final markerSpan = textSpan.children!.whereType<TextSpan>().last;
       expect(markerSpan.text, '\u00a0٣ ');
       expect(markerSpan.style?.color, AppTheme.classicAyahMarker);
-      expect(markerSpan.style?.fontWeight, FontWeight.w600);
+      expect(markerSpan.style?.fontWeight, FontWeight.w500);
       expect(
         markerSpan.style?.fontSize,
         greaterThanOrEqualTo(textSpan.style!.fontSize! * 0.85),
